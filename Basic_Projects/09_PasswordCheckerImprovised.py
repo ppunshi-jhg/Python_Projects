d63001b4679d26366e1fe80e4c95238edc8a946d
ppunshi <ppunshi@jhg.com.au>
2025-07-20 12:03:22 +1000

    adding 9th project improvised version

--- Basic_Projects/09_PasswordCheckerImprovised.py
+++ Basic_Projects/09_PasswordCheckerImprovised.py
@@ -3,9 +3,21 @@ import string
 
 def check_password(password: str) -> str:
     password = password.strip()
-    
-    with open('commonpasswords.txt', 'r') as file:
-        common_passwords = file.read().splitlines()
-        print(common_passwords)
+
+    with open('C:\\Users\\arora\\OneDrive - John Holland Group\\Github Repos\\Python Projects\\Basic_Projects\\commonpasswords.txt', 'r') as file:
+        common_passwords = [line.strip() for line in file if line.strip()]
         
-check_password("password")
+        
+    if password in common_passwords:
+        return f"Password is too common: {password}. Please choose a different password."
+    
+def main() -> str:
+    user_password = input(f"Enter your password to check if its common: ")
+    result = check_password(user_password)
+    if result:
+        print(result)
+    else:
+        print("Password is not common. You can use it.")
+        
+if __name__ == "__main__":
+    main()
--- Basic_Projects/test.py
+++ Basic_Projects/test.py
@@ -1,19 +1,5 @@
-import qrcode
+list1= [1,2,3,4,5]
 
-class QrCodeGenerator:
-    def __init__(self, size, padding):
-        self.qr = qrcode.QRCode(box_size=size, border=padding)
-        
-    def qrcode_create(self, filename: str, fg: str, bg: str):
-        data = input(f"Enter the data to encode in QR code: ")
-        
-        self.qr.add_data(data)
-        self.qr.make(fit=True)
-        img = self.qr.make_image(fill_color=fg, back_color=bg)
-        img.save(filename)
-        print(f"QR code saved as {filename}")
-        
+result = [l*2 for l in list1 if l % 2 == 0]
 
-if __name__ == "__main__":
-    qr_generator = QrCodeGenerator(size=30, padding=4)
-    qr_generator.qrcode_create(filename="my_qr_code.png", fg="black", bg="white")
+print(result)
